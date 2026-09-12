aa243efd7544c27381fdb6bef779a33931801edf
sionamanda3-create <sionamanda3@gmail.com>
2026-09-12 14:36:58 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFLbfQRwgANDYt8CfmV2aSSUW9ng1KIoY",
+  authDomain: "media-k3-pemesinan.firebaseapp.com",
+  projectId: "media-k3-pemesinan",
+  storageBucket: "media-k3-pemesinan.firebasestorage.app",
+  messagingSenderId: "32001771733",
+  appId: "1:32001771733:web:397c253906b19b42f5fa5c"
+};
